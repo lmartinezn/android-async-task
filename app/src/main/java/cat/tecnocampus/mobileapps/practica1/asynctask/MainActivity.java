922f56e3adc393c66155028ca61516993e190691
lmartinezn <lmartinezn@edu.tecnocampus.cat>
2019-04-30 11:56:03 +0200

    Added method to show all user's names

--- app/src/main/java/cat/tecnocampus/mobileapps/practica1/asynctask/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/practica1/asynctask/MainActivity.java
@@ -1,13 +1,14 @@
 package cat.tecnocampus.mobileapps.practica1.asynctask;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.TextView;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity implements HttpHelper.AsyncResponse{
+public class MainActivity extends AppCompatActivity implements HttpHelper.AsyncResponse {
     private TextView jsonView;
 
     @Override
@@ -26,18 +27,38 @@ public class MainActivity extends AppCompatActivity implements HttpHelper.AsyncR
     @Override
     public void setAsyncContentResult(String json) {
         //jsonView.setText(json);
-        jsonView.setText(getFirstUser(json));
+        //jsonView.setText(getFirstUser(json));
+        jsonView.setText(getAllNames(json));
     }
 
-    private String getFirstUser(String json){
-        try{
+    private String getFirstUser(String json) {
+        try {
             JSONArray users = new JSONArray(json);
             return users.get(0).toString();
-        }catch (Exception ex){
+        } catch (Exception ex) {
             Log.d("SwA", "JSON exception");
             ex.printStackTrace();
         }
 
         return "";
+    }
+
+    private String getAllNames(String json) {
+        String result = "";
+
+        try {
+            JSONArray users = new JSONArray(json);
+
+            for (int i = 0; i < users.length(); i++) {
+                JSONObject user = users.getJSONObject(i);
+                result = result + user.getString("name") + "\n"; /*Si no sabem si existeix
+                un camp "name" o no, podem utilitzar el mètode has per evitar excepcions*/
+            }
+        } catch (Exception ex) {
+            Log.d("SwA", "JSON exception");
+            ex.printStackTrace();
+        }
+
+        return result;
     }
 }
